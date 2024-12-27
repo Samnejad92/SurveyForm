c6d86669f238197b72ca46c28c30be6b93c36fd7
Samnejad92 <m.samnejad92@gmail.com>
2024-12-27 13:24:00 +0330

    Add project files.

---
+++ SurveyForm/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function displayContent(event, contentNameID) {
+
+    let content =
+        document.getElementsByClassName("contentClass");
+    let totalCount = content.length;
+
+    // Loop through the content class
+    // and hide the tabs first
+    for (let count = 0;
+        count < totalCount; count++) {
+        content[count].style.display = "none";
+    }
+
+    let links =
+        document.getElementsByClassName("linkClass");
+    totalLinks = links.length;
+
+    // Loop through the links and
+    // remove the active class
+    for (let count = 0;
+        count < totalLinks; count++) {
+        links[count].classList.remove("active");
+    }
+
+    // Display the current tab
+    document.getElementById(contentNameID)
+        .style.display = "block";
+
+    // Add the active class to the current tab
+    event.currentTarget.classList.add("active");
+}
